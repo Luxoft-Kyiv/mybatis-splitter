cae38a4e78994efc1455e3e0452ce37e5e9fb69e
Vitalii Tymchyshyn <tivv00@gmail.com>
2014-01-10 11:17:43 +0200

    Add mock test for batching (not working right now)

--- src/test/java/com/luxoft/mybatis/splitter/UpdateSplitterPluginTest.java
+++ src/test/java/com/luxoft/mybatis/splitter/UpdateSplitterPluginTest.java
@@ -22,20 +22,34 @@ import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.unitils.UnitilsJUnit4TestClassRunner;
+import org.unitils.easymock.annotation.Mock;
 
 import java.io.IOException;
-import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.easymock.EasyMock.expect;
+import static org.unitils.easymock.EasyMockUnitils.replay;
+
 /**
  * @author Vitalii Tymchyshyn
  */
+@RunWith(UnitilsJUnit4TestClassRunner.class)
 public class UpdateSplitterPluginTest {
+    @Mock
+    Connection connection;
+
+    @Mock
+    PreparedStatement statement;
+
     @Test
     public void splitterTestSimple() throws IOException, SQLException {
         splitterTest(ExecutorType.SIMPLE);
@@ -52,11 +66,47 @@ public class UpdateSplitterPluginTest {
         SqlSession sqlSession = sqlSessionFactory.openSession(execType);
         sqlSession.insert("makeTable");
         sqlSession.flushStatements();
+        doInsert(sqlSession);
+        Assert.assertEquals(Arrays.asList("first", "second", "third"), sqlSession.selectList("get"));
+        sqlSession.insert("dropTable");
+        sqlSession.flushStatements();
+        sqlSession.close();
+    }
+
+    private void doInsert(SqlSession sqlSession) {
         Map<String, Object> param = new HashMap<String, Object>();
         param.put("first", "first");
         param.put("other", Arrays.asList("second", "third"));
         sqlSession.insert("multy", param);
         sqlSession.flushStatements();
+    }
+
+    @Test
+    public void mockTest() throws IOException, SQLException {
+        expect(connection.getAutoCommit()).andStubReturn(false);
+        expect(connection.prepareStatement("insert into test values(?)")).andReturn(statement);
+        statement.setString(1, "first");
+        statement.addBatch();
+        expect(connection.prepareStatement("insert into test values(?)")).andReturn(statement);
+        statement.setString(1, "second");
+        statement.addBatch();
+        expect(connection.prepareStatement("insert into test values(?)")).andReturn(statement);
+        statement.setString(1, "third");
+        statement.addBatch();
+        expect(statement.executeBatch()).andStubReturn(new int[]{1});
+        statement.close();
+        statement.close();
+        statement.close();
+        connection.setAutoCommit(true);
+        connection.rollback();
+        connection.close();
+
+        replay();
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(
+                Resources.getResourceAsStream("configuration.xml"));
+        SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH, connection);
+        doInsert(sqlSession);
         sqlSession.close();
     }
 }
